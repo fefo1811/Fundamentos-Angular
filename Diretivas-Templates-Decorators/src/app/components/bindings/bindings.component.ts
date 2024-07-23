062d24259f0d3828d8ad5a647f337252916c0f6c
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-23 11:55:55 -0300

    Angular: Attribute & CSS Style Binding

--- Diretivas-Templates-Decorators/src/app/components/bindings/bindings.component.ts
+++ Diretivas-Templates-Decorators/src/app/components/bindings/bindings.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./bindings.component.css']
 })
 export class BindingsComponent {
+  // Aula 1 e 2: Property Binding e Event Binding
   inputText: string = "Texto passado via Binding";
   inputType: string = "text";
   isDisabled: boolean = false;
@@ -17,5 +18,34 @@ export class BindingsComponent {
   public handleInputKeyup(event: KeyboardEvent) {
     const textoAtual = (event.target as HTMLInputElement).value; // O target é variável. Ele pode indicar que há uma propriedade value ou não. Portanto deverá ser feito o Casting com HTMLInputElement
     console.log(textoAtual);
+  }
+// =============================================
+
+  // Aula 3: Attribute Binding
+  textoBotao: string = "Texto do Botão";
+  buttonDisable: boolean = false;
+
+  buttonClick() {
+    this.textoBotao = "Título alterado";
+    this.buttonDisable = !this.buttonDisable;
+  }
+// =============================================
+
+  // Aula 4: CSS Binding
+  widthButton1: string = '100px';
+  widthButton2: string = '130';
+  bgColorButton: string = 'aqua';
+  switcher: boolean = false;
+
+  stylesObj = {
+    width: '160px',
+    backgroundColor: 'yellow',
+  }
+
+  updateStylesObj() {
+    this.stylesObj = {
+      width: '200px',
+      backgroundColor: 'purple',
+    }
   }
 }
