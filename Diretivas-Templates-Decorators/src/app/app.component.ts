ad4a1ca5eb022d66d0fea86aa0e51c34e3d2754b
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-25 15:58:56 -0300

    Angular: Decorator @Input

--- Diretivas-Templates-Decorators/src/app/app.component.ts
+++ Diretivas-Templates-Decorators/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  cardPlanType: string = "Simples";
+  cardPlanPrice: number = 100;
 }
--- Diretivas-Templates-Decorators/src/app/app.module.ts
+++ Diretivas-Templates-Decorators/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CardsModule} from "./components/cards/cards.module";
 
 import { AppComponent } from './app.component';
 import { BindingsComponent } from './components/bindings/bindings.component';
+
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { BindingsComponent } from './components/bindings/bindings.component';
   ],
   imports: [
     BrowserModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
