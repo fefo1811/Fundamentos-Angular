d63fc21d4842a0bba5da71e94577cd2df7520eb2
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-08-05 14:28:30 -0300

    Projeto Cards: Criação do CardComponent e Simulando fluxo de uma chamda HTTP com @Output

--- Projetos/Projeto-Cards/src/app/app.component.ts
+++ Projetos/Projeto-Cards/src/app/app.component.ts
@@ -9,7 +9,12 @@ export class AppComponent {
   textoBotao1: string = "Felipe";
   textoBotao2: string = "Fefo";
 
-  buttonEmitterCatcher(msg: string): void {
-    console.log(`${msg} <----  Veio do ButtonComponent`);
+  onCardButtonClicked() {
+    console.log("Simulando fluxo de uma chamada HTTP")
+    /**
+     * 1. Um evento foi emitido do ButtonComponent
+     * 2. Foi enviado para o CardComponent que emitiu um evento void para o AppComponent
+     * 3. No AppComponent o Emitter é recebido e é onde é realizada a chamada HTTP
+     */
   }
 }
