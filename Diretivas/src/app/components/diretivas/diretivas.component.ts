4fb193a6dae8012a24c920169885b5bc08f3d4c6
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-08-24 16:04:59 -0300

    Angular: ngFor

--- Diretivas/src/app/components/diretivas/diretivas.component.ts
+++ Diretivas/src/app/components/diretivas/diretivas.component.ts
@@ -15,4 +15,21 @@ export class DiretivasComponent {
   isTruthy() {
     return "";
   }
+
+
+  // ngFor
+  // listPessoas = ['Felipe', 'João', true, 1000]
+  listPessoas = [
+    { name: 'Felipe da Silva', age: 23 },
+    { name: 'Fulano da Silva', age: 34 },
+    { name: 'Valentin da Silva', age: 8 },
+    { name: 'Lorenzo da Silva', age: 3 },
+  ]
+
+  personSelectedIndex: number | undefined;
+
+  selectPerson(i: number) {
+    console.log(i)
+    this.personSelectedIndex = i;
+  }
 }
