4df838191154e7dd1fdd287b7ee84abccab1d945
fefo1811 <felipedasr3232@gmail.com>
2024-10-09 09:56:04 -0300

    Angular: Two-Way Data Binding

--- Diretivas/src/app/components/two-way/two-way.component.ts
+++ Diretivas/src/app/components/two-way/two-way.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-two-way',
@@ -7,8 +7,43 @@ import { Component } from '@angular/core';
 })
 export class TwoWayComponent {
   name: string = 'Felipe';
-  
+
+
   handleInputChange(event: string) {
     this.name = event;
   }
+
+
+
+
+  // Treinando ngIf com ngTemplate
+  isTexto: boolean = true;
+
+  // showText() {
+  //   this.isTexto = !this.isTexto;
+  //   console.log(this.isTexto);
+  // }
+
+
+
+  // // Treinando @Input com Get e Set
+  // private _variavelPrivada: string = '';
+
+  // // Método SET
+  // @Input() set variavelPrivada(value: string) {
+  //   this._variavelPrivada = value;
+  // }
+
+
+  // // Método GET
+  // get variavelPrivada(): string {
+  //   return this._variavelPrivada;
+  // }
+
+  // // Treinando @output
+  // @Output() emissor = new EventEmitter<string>();
+  // emitir() {
+  //   this.emissor.emit(this.variavelPrivada);
+  // }
+
 }
